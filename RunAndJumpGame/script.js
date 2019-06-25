78f3fcfa813dba5e4d3a880ffa1e1ae726bd2990
mavioux <pakmarios@gmail.com>
2019-06-25 14:28:37 +0300

    Setup and Draw Function initialized

--- RunAndJumpGame/script.js
+++ RunAndJumpGame/script.js
@@ -31,3 +31,85 @@ class Obstacle {
         this.speed = canvas.width * 5 / canvasWidth;
     }
 }
+
+let score = 0;
+let gameover = false;
+let start = true;
+let paused = true;
+let fps = 60;
+let player;
+let obstacle;
+
+//Call for our function to execute when the page loads
+document.addEventListener('DOMContentLoaded', setupCanvas);
+
+//Setup
+function setupCanvas() {
+
+    //Reference to the html canvas
+    canvas = document.getElementById('my-canvas');
+
+    //Get methods for canvas manipulation
+    ctx = canvas.getContext('2d');
+
+    //Code I found to fix scaling in smaller devices
+    ctx.canvas.width  = window.innerWidth * canvasWidth / 1920;
+    ctx.canvas.height = window.innerHeight * canvasHeight / 1080;
+
+    player = new Player();
+
+    //Initialize the player Rectanglect
+    ctx.fillstyle = '#551a8b';  //purple
+    ctx.fillRect
+
+    //Handle Player Input
+    document.addEventListener('keydown', playerJump);
+    document.addEventListener('keydown', startF);
+
+    //Draw the elements for the first time
+    draw();
+}
+
+//Draw function
+function draw() {
+
+    //Clear the previous frame canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //Black Background
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    //Draw the player Rectangle
+    ctx.fillstyle = 'white';  //purple
+    ctx.fillRect = (300, 300 , player.width, player.height);
+    
+    //Draw the obstacle Rectangle if there is one
+    if (obstacle != null) {
+        ctx.fillstyle = '#ffff00';  //purple
+        ctx.fillRect = (obstacle.x, obstacle.y , obstacle.width, obstacle.height);
+    }
+
+    
+}
+
+//PlayerJump Function
+function playerJump () {
+    
+}
+
+//Start Function
+function startF(key) {
+    if(key.keyCode === 32 && start === true) {
+        start = false;
+        window.requestAnimationFrame(gameloop);
+    }
+}
+
+//Gameloop
+function gameloop() {
+    if(!paused) {
+        update();
+        draw();
+    }
+}
