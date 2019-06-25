0cc18dec847764d9c36099e804139b5e7ca2ff90
mavioux <pakmarios@gmail.com>
2019-06-25 19:56:22 +0300

    managed to make it jump

--- RunAndJumpGame/script.js
+++ RunAndJumpGame/script.js
@@ -4,6 +4,7 @@ let playerWidth = 50
 let playerHeight = 200
 let obstacleWidth = 50
 let obstacleHeight = 50
+let playerLevelY;
 
 
 // Reference to the canvas element
@@ -14,32 +15,36 @@ let ctx;
 
 class Player {
     constructor() {
-        this.width = canvas.width * playerWidth / canvasWidth;
-        this.height = canvas.height * playerHeight / canvasHeight;
-        this.x = canvas.width * 200 / canvasWidth;
-        this.y = canvas.height * (canvas.height - this.height) / canvasHeight;
-        this.speed = canvas.height * 2 / canvasHeight;
+        this.width = ctx.canvas.width * playerWidth / canvasWidth;
+        this.height = ctx.canvas.height * playerHeight / canvasHeight;
+        this.x = ctx.canvas.width * 200 / canvasWidth;
+        this.y = (ctx.canvas.height - this.height);
+        this.speed = ctx.canvas.height * 2 / canvasHeight;
+        playerLevelY = (ctx.canvas.height - this.height);
     }
 }
 
 class Obstacle {
     constructor(speed, height) {
-        this.width = canvas.width * obstacleWidth / canvasWidth;
-        this.height = canvas.height * obstacleHeight / canvasHeight;
-        this.x = canvas.width;
-        this.y = canvas.height * height / canvasHeight;
-        this.speed = canvas.width * 5 / canvasWidth;
+        this.width = ctx.canvas.width * obstacleWidth / canvasWidth;
+        this.height = ctx.canvas.height * obstacleHeight / canvasHeight;
+        this.x = ctx.canvas.width;
+        this.y = ctx.canvas.height * height / canvasHeight;
+        this.speed = ctx.canvas.width * 5 / canvasWidth;
     }
 }
 
 let score = 0;
 let gameover = false;
-let start = true;
-let paused = true;
+// let start = true;
+let paused = false;
 let fps = 60;
 let player;
 let obstacle;
-let scoreFontSize 
+let scoreFontSize;
+let jumping = false;
+let falling = false;
+let playing = false;
 
 //Call for our function to execute when the page loads
 document.addEventListener('DOMContentLoaded', setupCanvas);
@@ -58,17 +63,13 @@ function setupCanvas() {
     ctx.canvas.height = window.innerHeight * canvasHeight / 1080;
 
     //Set Score Font size
-    scoreFontSize = canvas.width / canvas.height * 30;
+    scoreFontSize = canvas.width / canvas.height * 20;
 
     player = new Player();
 
-    //Initialize the player Rectanglect
-    ctx.fillstyle = '#551a8b';  //purple
-    ctx.fillRect
-
     //Handle Player Input
     document.addEventListener('keydown', playerJump);
-    document.addEventListener('keydown', startF);
+    // document.addEventListener('keydown', startF);
 
     //Draw the elements for the first time
     draw();
@@ -78,11 +79,11 @@ function setupCanvas() {
 function draw() {
 
     //Clear the previous frame canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
     //Black Background
     ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     
     //Draw the player Rectangle
     ctx.fillStyle = 'white';  //purple
@@ -98,27 +99,66 @@ function draw() {
     ctx.fillStyle = 'white';
     ctx.font = scoreFontSize + "px Arial";
     ctx.textAlign = "center";
-    ctx.fillText(score, canvas.width / 2, canvas.width * 25 / canvasHeight);
+    ctx.fillText(score, ctx.canvas.width / 2, ctx.canvas.width * 40 / canvasHeight);
     
 }
 
 //PlayerJump Function
-function playerJump () {
-    
+function playerJump (key) {
+
+    if(playing === false) {
+        playing = true;
+        window.requestAnimationFrame(gameloop);
+    }
+
+    if(key.keyCode === 32 && falling === false && jumping === false) {
+        jumping = true
+        // alert('i pressed space');
+    }
+}
+
+//Update Function
+function update() {
+
+    //if jump was pressed move the player model up in the air, until it reaches maximum height
+    if(jumping) {
+        // alert('hi');
+        if(player.y > playerLevelY - ctx.canvas.height / canvasHeight * 500) {
+            player.y -= 30;
+        }
+        else {
+            jumping = false;
+            falling = true;
+        }
+    }
+    else if (falling) {
+        if(player.y < playerLevelY) {
+            player.y += 20;
+        }
+        else {
+            jumping = false;
+            falling = false;
+        }
+    }
+
 }
 
 //Start Function
-function startF(key) {
-    if(key.keyCode === 32 && start === true) {
-        start = false;
-        window.requestAnimationFrame(gameloop);
-    }
-}
+// function startF(key) {
+//     if(key.keyCode === 32 && start === true) {
+//         start = false;
+//         window.requestAnimationFrame(gameloop);
+//     }
+// }
 
 //Gameloop
 function gameloop() {
     if(!paused) {
         update();
         draw();
+    }
+
+    if(!gameover) {
+        requestAnimationFrame(gameloop);
     }
 }
